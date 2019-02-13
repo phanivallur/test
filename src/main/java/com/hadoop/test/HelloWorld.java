fb4801015a7a1b7109da794a1a4bc4166707560c
phanivallur <33691241+phanivallur@users.noreply.github.com>
2019-02-13 18:49:51 +0530

    commit 1

---
+++ src/main/java/com/hadoop/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hadoop.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Sujatha.. I'm glad that it's finally working!");
+	}
+
+}
